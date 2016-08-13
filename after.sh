8d3ef0ebdd09b7d77f678b6924e82a6f5bb8d91d
Javier Carmona <javierscarmona@gmail.com>
2016-08-13 13:56:34 -0400

    Fix default database name

--- after.sh
+++ after.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "Importing `wp-starter` database"
+echo "Importing `wp_starter` database"
 
-mysql -u homestead -psecret wp-starter < /home/vagrant/wp-starter/database.sql
+mysql -u homestead -psecret wp_starter < /home/vagrant/wp-starter/database.sql
